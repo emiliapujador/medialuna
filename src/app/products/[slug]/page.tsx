b70b871b3b6b48a066f382b548f2f3da0c09498c
emiliapujador <emiliapujador@gmail.com>
2024-11-12 12:14:53 +0000

    pdp wip

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,6 +1,19 @@
 import Image from "next/image";
 import Maicenitas from "@/images/homecategories1.png";
 import { AddtoCartButton } from "@/components/AddToCart";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default async function Page({
   params,
@@ -25,9 +38,58 @@ export default async function Page({
           <p className="mb-2">Short description</p>
           <p className="text-sm mb-3">£XX</p>
           <p className="text-sm mb-2">Quantity:</p>
-
-          <AddtoCartButton />
+          <div className="mb-5">
+            <AddtoCartButton />
+          </div>
+          <div className="mb-4">
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur. Pulvinar neque senectus
+              massa sit turpis risus cursus id non nulla facilisis. Lorem ipsum
+              dolor sit amet consectetur.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet
+              consectetur.
+            </p>
+            <p>
+              Pulvinar neque senectus massa sit turpis risus cursus id non nulla
+              facilisis. Pulvinar neque senectus massa sit turpis risus cursus
+              id non nulla facilisis.
+            </p>
+          </div>
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger className=" text-[#373737] font-normal text-base	">
+                INGREDIENTS
+              </AccordionTrigger>
+              <AccordionContent className="text-base	">
+                Pulvinar neque senectus massa sit turpis risus cursus id non
+                nulla facilisis.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+              <AccordionTrigger className=" text-[#373737] font-normal text-base	 ">
+                HOW TO STORE THE PRODUCT
+              </AccordionTrigger>
+              <AccordionContent className="text-base	">
+                Pulvinar neque senectus massa sit turpis risus cursus id non
+                nulla facilisis.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </div>
+      </div>
+      <div className="flex flex-row container mx-auto">
+        <Carousel>
+          <CarouselContent>
+            <CarouselItem>...</CarouselItem>
+            <CarouselItem>...</CarouselItem>
+            <CarouselItem>...</CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
     </main>
   );
